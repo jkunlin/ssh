df42f8efeb7952c702d0e19a7639adaffa47549d
jkunlin <jkunlin@gmail.com>
2014-05-13 15:37:23 +0800

    many-to-one journal article, empty

---
+++ src/com/bean/Article.java
@@ -0,0 +1,20 @@
+package com.bean;
+
+public class Article {
+	private Journal journal;
+	private Integer article_id;
+	
+	public Journal getJournal() {
+		return journal;
+	}
+	public void setJournal(Journal journal) {
+		this.journal = journal;
+	}
+	public Integer getArticle_id() {
+		return article_id;
+	}
+	public void setArticle_id(Integer article_id) {
+		this.article_id = article_id;
+	}
+	
+}
--- src/com/bean/Journal.java
+++ src/com/bean/Journal.java
@@ -1,7 +1,12 @@
 package com.bean;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Journal {
 	private Integer journal_id;
+	private Set<Article> articles = new HashSet<Article>();
+	
 	
 	public Integer getJournal_id() {
 		return journal_id;
@@ -9,5 +14,12 @@ public class Journal {
 	public void setJournal_id(Integer journal_id) {
 		this.journal_id = journal_id;
 	}
+	public Set<Article> getArticles() {
+		return articles;
+	}
+	public void setArticles(Set<Article> articles) {
+		this.articles = articles;
+	}
+	
 	
 }
